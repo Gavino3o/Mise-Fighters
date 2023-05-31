efdfeed071201ce60222a54a9b0f71c44dc3c094
Gavino3o <gavinchiam@gmail.com>
2023-06-01 01:30:43 +0800

    Implement Chef Blink and Slice

--- Vinners/Assets/_Scripts/Units/Character Scripts/Head Chef/ChefCastCharacter.cs
+++ Vinners/Assets/_Scripts/Units/Character Scripts/Head Chef/ChefCastCharacter.cs
@@ -8,34 +8,82 @@ public class ChefCastCharacter : CastCharacter
     #region Slice skill
     [Header("Slice skill")]
     [SerializeField] private GameObject sliceSpellPrefab;
-
+    [SerializeField] private float sliceAngle = 30;
     public void onSkill()
     {
         if (!IsOwner) return;
+
+        if (base.canCast[0])
+        {
+            StartCoroutine(Cooldown(0));
+            CastSliceSkill();
+            Debug.Log("Spell casted");
+        }
+        else
+        {
+            Debug.Log("Spell is on cooldown");
+        }
     }
 
     [ServerRpc]
     public void CastSliceSkill()
     {
-        //Hardcoded everything for now, decide injection later.
-        //Consider using same slice for both slices.
-        Vector3 positionOffset1 = Vector3.zero;
-        Vector3 positionOffset2 = Vector3.zero;
-        Quaternion rotationOffset1 = Quaternion.identity; //120 degrees
-        Quaternion rotationOffset2 = Quaternion.identity;
+        // Consider implementing using one slice and using animation destroy.
+        Quaternion rotationOffset1 = Quaternion.Euler(0, sliceAngle, 0);
+        Quaternion rotationOffset2 = Quaternion.Euler(0, -sliceAngle, 0);
+        GameObject firstSlice = Instantiate(sliceSpellPrefab, transform.position + transform.up, transform.rotation * rotationOffset1);
+        GameObject secondSlice = Instantiate(sliceSpellPrefab, transform.position + transform.up, transform.rotation * rotationOffset2);
 
-        GameObject slice1 = Instantiate(sliceSpellPrefab, transform.position + positionOffset1, transform.rotation * rotationOffset1);
-        GameObject slice2 = Instantiate(sliceSpellPrefab, transform.position + positionOffset2, transform.rotation * rotationOffset2);
-
-        slice1.GetComponent<Lifetime>().lifetime = spellData[0].duration;
-        slice2.GetComponent<Lifetime>().lifetime = spellData[0].duration;
-        slice1.GetComponent<EnemyDamager>().damage = spellData[0].damage * character.currAttack;
-        slice2.GetComponent<EnemyDamager>().damage = spellData[0].damage * character.currAttack;
-
-        ServerManager.Spawn(slice1);
-        ServerManager.Spawn(slice2);
+        firstSlice.GetComponent<Lifetime>().lifetime = spellData[0].duration;
+        secondSlice.GetComponent<Lifetime>().lifetime = spellData[0].duration;
+        firstSlice.GetComponent<EnemyDamager>().damage = spellData[0].damage * character.currAttack;
+        secondSlice.GetComponent<EnemyDamager>().damage = spellData[0].damage * character.currAttack;
+        ServerManager.Spawn(firstSlice);
+        ServerManager.Spawn(secondSlice);
         Debug.Log($"{spellData[0].spellName} casted");
     }
 
+    #endregion
+
+    #region Chef Blink
+    [Header("Blink Skill")]
+
+    public float blinkDistance = 2.5f;
+    public void OnDash()
+    {
+        if (!IsOwner) return;
+        if (canCast[1])
+        {
+            StartCoroutine(Cooldown(1));
+            StartCoroutine(Blink());
+            Debug.Log($"{spellData[1].spellName} casted");
+        }
+        else
+        {
+            Debug.Log("Spell is on cooldown");
+        }
+    }
+
+    public IEnumerator Blink()
+    {
+        Vector2 blinkDirection = input.targetDirection;
+
+        // TODO: Add layering for obstacle layer.
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, blinkDirection, blinkDistance);
+        movement.interrupted = true;
+        if (hit.collider == null)
+        {
+            Vector2 newPosition = (Vector2) transform.position + blinkDirection * blinkDistance;
+            transform.position = newPosition;
+        } 
+        else
+        {
+            float obstacleDistance = hit.distance;
+            Vector2 newPosition = (Vector2) transform.position + blinkDirection * (obstacleDistance - 0.5f);
+            transform.position = newPosition;
+        }
+        yield return new WaitForSeconds(spellData[1].duration);
+        movement.interrupted = false;
+    }
     #endregion
 }
