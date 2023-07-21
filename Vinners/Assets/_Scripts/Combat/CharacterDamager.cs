40e896bb5e8b888db01b3f89f3a81bd9d8f3839c
alvinlim277 <alvinlim9815@gmail.com>
2023-07-21 16:56:00 +0800

    Fix bugs
    - Butcher dash bug
    - Exploder enemy bug

--- Vinners/Assets/_Scripts/Combat/CharacterDamager.cs
+++ Vinners/Assets/_Scripts/Combat/CharacterDamager.cs
@@ -5,19 +5,20 @@ using FishNet.Object;
 public class CharacterDamager : NetworkBehaviour
 {
     public float damage;
-    private Character chr = null;
+    // private Character chr = null;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        chr = other.gameObject.GetComponent<Character>();
-        if (chr != null) DealDamage();
+        Character chr = other.gameObject.GetComponent<Character>();
+        DealDamage(chr);
     }
 
+    
     [ServerRpc(RequireOwnership = false)]
-    private void DealDamage()
+    private void DealDamage(Character chr)
     {
-        if (chr == null) return;
-        chr.TakeDamage(damage);
+        if (chr != null) chr.TakeDamage(damage);
     }
+    
 
 }
